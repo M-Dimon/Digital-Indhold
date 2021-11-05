348ccf0900e9b3d36f7356f4eb19d025530ab9c9
Julie Jalsøe <juliejalsoe4@gmail.com>
2021-11-05 10:36:24 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,23 +1,45 @@
-const setActive = (el, active) => {
-  const formField = el.parentNode.parentNode
-  if (active) {
-    formField.classList.add('form-field--is-active')
-  } else {
-    formField.classList.remove('form-field--is-active')
-    el.value === '' ?
-      formField.classList.remove('form-field--is-filled') :
-      formField.classList.add('form-field--is-filled')
-  }
-}
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
 
-[].forEach.call(
-  document.querySelectorAll('.form-field__input, .form-field__textarea'),
-  (el) => {
-    el.onblur = () => {
-      setActive(el, false)
-    }
-    el.onfocus = () => {
-      setActive(el, true)
-    }
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
   }
-)
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
